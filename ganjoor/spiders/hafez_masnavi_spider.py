b755fd6e92dfede5d0abc476418cc1834bdf8c5f
amirmasoud <amirmasood33@gmail.com>
2017-07-26 15:21:54 +0430

    Initial commit

---
+++ ganjoor/spiders/hafez_masnavi_spider.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class HafezMasnaviSpider(scrapy.Spider):
+    name = "hafez_masnavi"
+    allowed_domains = ["ganjoor.net"]
+    start_urls = ['https://ganjoor.net/hafez/masnavi']
+
+    def parse(self, response):
+        sh = {}
+        for index, poem in enumerate(response.css('div.poem>article>div.b')):
+            sh[index] = {
+                'm1': poem.css('div.m1>p::text').extract_first(),
+                'm2': poem.css('div.m2>p::text').extract_first(),
+            }
+        yield sh
